153add82aecf3ca57c59b845254bd8b4de76b032
Bernadette Diemer <diemer.lulu@gmail.com>
2021-10-07 20:44:56 +0200

    completed the challenge, embeded the review-form in the restaurant-show

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,14 +4,15 @@ class ReviewsController < ApplicationController
     @review = Review.new
   end
 
- def create
+  def create
     @review = Review.new(review_params)
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review.restaurant = @restaurant
+
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
-      render :new
+      render 'restaurants/show'
     end
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@ puts "Creating restaurants..."
 
 10.times do
   restaurant = Restaurant.new( name: Faker::Restaurant.name, address: Faker::Address.city, category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+  restaurant.save
   puts "Created #{restaurant.name}"
 end
 
